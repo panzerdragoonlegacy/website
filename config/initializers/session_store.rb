1b7280726c7430fb5af5733f1893f6675bf37247
Chris Alley <chris@chrisalley.info>
2012-10-26 21:50:50 +1300

    Created application.yml

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TheWillOfTheAncients::Application.config.session_store :cookie_store, :key => '_TheWillOfTheAncients_session'
+TheWillOfTheAncients::Application.config.session_store :cookie_store, :key => APP_CONFIG['session']['key']
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
