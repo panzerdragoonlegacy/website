a72b4d62cce9f02d33d40458566d5b000fd1dff8
Chris Alley <chris@chrisalley.info>
2013-01-23 23:29:19 +1300

    Add correct alt, width, and height attributes to all images.

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,2 +1,11 @@
 module PicturesHelper
+  
+  def picture_tag(picture, style)
+    image_file = Paperclip::Geometry.from_file(picture.picture.path(style))
+    image_tag(picture.picture.url(style), 
+      :alt => picture.name, 
+      :width => image_file.width.to_i.to_s, 
+      :height => image_file.height.to_i.to_s)
+  end
+  
 end
