47c4f90c87da319a984104fb5971be182121b228
Chris Alley <chris@chrisalley.info>
2014-04-12 14:02:44 +1200

    Update contributors validation message

--- app/models/concerns/contributable.rb
+++ app/models/concerns/contributable.rb
@@ -4,6 +4,6 @@ module Contributable
   included do
     has_many :contributions, as: :contributable, dependent: :destroy
     has_many :dragoons, through: :contributions
-    validates :dragoons, length: { minimum: 1 }
+    validates :dragoons, length: { minimum: 1, too_short: "(contributors) count must be at least %{count}" }
   end
 end
