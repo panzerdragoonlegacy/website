a4c4c8e54b49b6bbf96a0850b1a0a5657a2c6248
Chris Alley <chris@chrisalley.info>
2015-01-15 14:50:41 +1300

    Move Twitter configuration into News Entry model

--- app/models/news_entry.rb
+++ app/models/news_entry.rb
@@ -1,21 +1,29 @@
 class NewsEntry < ActiveRecord::Base
   include Sluggable
-  
+
   belongs_to :dragoon
-  
+
   validates :name, presence: true, length: { in: 2..55 }, uniqueness: true
   validates :content, presence: true
 
   before_save :publish_news_entry
-    
+
   def publish_news_entry
     # The first time the news entry is published:
     if self.published_at.blank? && self.publish
-      
+
       # Set published_at field in the database to the current datetime:
       self.published_at = DateTime.now
-      
+
       full_url = "http://www.thewilloftheancients.com/news/" + self.url
+
+      # Get Twitter authentication details from secrets.yml
+      Twitter.configure do |config|
+        config.consumer_key = Rails.application.secrets.twitter["consumer_key"]
+        config.consumer_secret = Rails.application.secrets.twitter["consumer_secret"]
+        config.oauth_token = Rails.application.secrets.twitter["oauth_token"]
+        config.oauth_token_secret = Rails.application.secrets.twitter["oauth_token_secret"]
+      end
       
       # Post the status update field + short URL to Twitter:
       client = Twitter::Client.new
@@ -23,7 +31,7 @@ class NewsEntry < ActiveRecord::Base
         client.update(self.name + " " + full_url)
       else
         client.update(self.name + ": " + full_url)
-      end    
+      end
     end
   end
 end
--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = Rails.application.secrets.twitter["consumer_key"]
-  config.consumer_secret = Rails.application.secrets.twitter["consumer_secret"]
-  config.oauth_token = Rails.application.secrets.twitter["oauth_token"]
-  config.oauth_token_secret = Rails.application.secrets.twitter["oauth_token_secret"]
-end
