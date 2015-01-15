9cc8b0f4fc883c73dde06371453d0e82bd6140d8
Chris Alley <chris@chrisalley.info>
2015-01-15 14:35:31 +1300

    Remove unused mail configuration

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => Rails.application.secrets.mail["user_name"],
-  :password             => Rails.application.secrets.mail["password"],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
