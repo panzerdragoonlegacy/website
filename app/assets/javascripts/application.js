0d72add060175f335c1a567c8b6243171cb248b3
Chris Alley <chris@chrisalley.info>
2015-01-16 12:46:09 +1300

    Fix broken deletion links by adding jquery_ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
-//= require jquery  
+//= require jquery
+//= require jquery_ujs
 //= require jquery_nested_form  
 //= require local_time
 //= require turbolinks
-//= require_self  
+//= require_self
 //= require_tree .
