58ee505331445cc0030faad40cd8894d7a84a2bf
Chris Alley <chris@chrisalley.info>
2015-05-27 20:47:00 +1000

    Increase length of saga name

--- app/models/saga.rb
+++ app/models/saga.rb
@@ -4,7 +4,7 @@ class Saga < ActiveRecord::Base
   belongs_to :encyclopaedia_entry
   validates :encyclopaedia_entry, presence: true, uniqueness: true
 
-  validates :name, presence: true, uniqueness: true, length: { in: 2..25 }
+  validates :name, presence: true, uniqueness: true, length: { in: 2..30 }
   validates :sequence_number, presence: true, numericality: {
     only_integer: true, greater_than: 0, less_than: 100 }
 end
--- spec/models/saga_spec.rb
+++ spec/models/saga_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Saga, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
-    it { should validate_length_of(:name).is_at_least(2).is_at_most(25) }
+    it { should validate_length_of(:name).is_at_least(2).is_at_most(30) }
     it { should validate_presence_of(:sequence_number) }
     it { should validate_numericality_of(:sequence_number).is_greater_than(0).
       is_less_than(100) }
