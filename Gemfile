c60b566a7d49d05c1832086673a247c711487ab6
Chris Alley <chris@chrisalley.info>
2015-07-12 19:27:00 +1000

    Update ruby version and deployment script

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.2.0'
 group :development do
   gem 'capistrano', '2.15.5' # Out of date
   gem 'puma', '~> 2.9.1'
+  gem 'rvm-capistrano', '1.5.6'
   gem 'spring', '~> 1.1.2'
   gem 'sqlite3', '~> 1.3.9'
   gem 'web-console', '~> 2.0.0'
@@ -18,10 +19,6 @@ end
 group :test do
   gem 'factory_girl_rails', '~> 4.4.1'
   gem 'shoulda-matchers', '~> 2.8.0'
-end
-
-group :production do
-  gem 'therubyracer', '~> 0.12.1'
 end
 
 gem 'bcrypt', '~> 3.1.7'
@@ -40,6 +37,7 @@ gem 'rails_autolink', '~> 1.1.5'
 gem 'sanitize', '~> 3.1.0'
 gem 'sass-rails', '~> 4.0.3'
 gem 'stringex', '~> 2.5.2'
+gem 'therubyracer', '~> 0.12.2'
 gem 'turbolinks', '~> 2.5.3'
 gem 'twitter', '~> 4.8.1' # Out of date
 gem 'uglifier', '>= 2.5.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,63 +1,42 @@
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
 set :application, "thewilloftheancients"
 
-role :web, "web412.webfaction.com"                          # Your HTTP server, Apache/etc
-role :app, "web412.webfaction.com"                          # This may be the same as your `Web` server
-role :db,  "web412.webfaction.com", :primary => true        # This is where Rails migrations will run
+role :web, "178.62.191.241"                # Your HTTP server, Apache/etc
+role :app, "178.62.191.241"                # This may be the same as your `Web` server
+role :db,  "178.62.191.241", primary: true # This is where Rails migrations will run
 
-set :user, "chrisalley"
-set :deploy_to, "/home/chrisalley/webapps/thewilloftheancients"
+set :user, "thewilloftheancients"
+set :deploy_to, "/home/thewilloftheancients/site"
 set :use_sudo, false
-set :default_environment, {
-  'PATH' => "#{deploy_to}/bin:$PATH",
-  'GEM_HOME' => "#{deploy_to}/gems",
-}
-default_run_options[:pty] = true
 
 set :scm, "git"
 set :repository,  "https://chrisalley@github.com/chrisalley/the-will-of-the-ancients.git"
 set :branch, "master"
 
-namespace :deploy do
-  desc "Restart nginx"
-  task :restart do
-    run "#{deploy_to}/bin/restart"
-  end
+set :rails_env, "production"
+set :rvm_ruby_string, '2.2.1@thewilloftheancients' # Defaults to 'default'
+set :rvm_type, :system
+set :rvm_path, '/usr/local/rvm'
 
-  desc "Symlink extra configs and folders."
-  task :symlink_extras do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
-    run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
-    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
-  end
-
-  desc "Setup script permissions."
-  task :setup_script_permissions do
-    run "chmod +x #{deploy_to}/current/scripts/adhoc_backup.sh"
-    run "chmod +x #{deploy_to}/current/scripts/set_permissions.sh"
-  end
-
-  desc "Setup shared directory."
-  task :setup_shared_directory do
-    run "mkdir #{shared_path}/assets"
-    run "mkdir #{shared_path}/config"
-    put File.read("config/examples/database.yml"), "#{shared_path}/config/database.yml"
-    put File.read("config/examples/application.yml"), "#{shared_path}/config/application.yml"
-    put File.read("config/examples/secrets.yml"), "#{shared_path}/config/secrets.yml"
-  end
-
-  namespace :assets do
-    desc "Precompile assets on local machine and upload them to the server."
-    task :precompile, roles: :web, except: {no_release: true} do
-      run_locally "bundle exec rake assets:precompile RAILS_ENV=production"
-      find_servers_for_task(current_task).each do |server|
-        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
-      end
-    end
-  end
+before "deploy:assets:precompile" do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
+  run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
 end
 
-after "deploy", "deploy:cleanup" # Keeps only the last 5 releases
-after "deploy:setup", "deploy:setup_shared_directory"
-after "deploy:update_code", "deploy:symlink_extras"
-after "deploy:create_symlink", "deploy:setup_script_permissions"
+after("deploy:migrate", "deploy:build_missing_paperclip_styles")
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, roles: :app, except: { no_release: true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles, roles: :app do
+  run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake paperclip:refresh:missing_styles"
+  end
+end
