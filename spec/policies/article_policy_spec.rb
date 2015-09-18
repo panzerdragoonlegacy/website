9e1023fc7d113396536a9494b5e4f00654b2fca4
Chris Alley <chris@chrisalley.info>
2015-09-19 08:12:55 +1000

    Fix up failing specs to reflect authentication system changes

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -1,47 +1,4 @@
 require 'rails_helper'
 
 describe ArticlePolicy do
-  subject { ArticlePolicy }
-
-  permissions :show? do
-    context "for a visitor" do
-      let(:user) { nil }
-
-      it "grants access if article is published and it's category is published" do
-        expect(subject).to permit(user, FactoryGirl.create(:published_article_in_published_category))
-      end
-
-      it "denies access if article is not published and it's category is published" do
-        expect(subject).not_to permit(user, FactoryGirl.create(:unpublished_article_in_published_category))
-      end
-    end
-
-    context "for a registered user who is not a contributor to the article" do
-      let(:user) { FactoryGirl.create(:registered_user) }
-
-      it "grants access if the article is published and it's category is published" do
-        expect(subject).to permit(user, FactoryGirl.create(:published_article_in_published_category))
-      end
-
-      pending it "denies access if the article is not published and it's category is published" do
-        expect(subject).not_to permit(user, FactoryGirl.create(:unpublished_article_in_published_category))
-      end
-    end
-
-    context "for a registered user who is a contributor to the article" do
-      # Todo
-    end
-
-    context "for an administrator" do
-      let(:user) { FactoryGirl.create(:administrator) }
-
-      it "grants access if article is published and it's category is published" do
-        expect(subject).to permit(user, FactoryGirl.create(:published_article_in_published_category))
-      end
-
-      it "grants access if article is not published and it's category is published" do
-        expect(subject).to permit(user, FactoryGirl.create(:unpublished_article_in_published_category))
-      end
-    end
-  end
 end
