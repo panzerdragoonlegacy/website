52d6bd74a2656b41090d67df0307aa8b2f2bbe43
Chris Alley <chris@chrisalley.info>
2015-10-03 12:03:35 +1000

    Write article policy specs and matchers for pundit

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :user do
+    factory :registered_user do
+      sequence(:email) { |n| "testuser#{n}@example.com" }
+      password "1234567A"
+      password_confirmation "1234567A"
+      confirmed_at Time.now
+
+      factory :contributor do
+        contributor_profile { FactoryGirl.create(:contributor_profile) }
+      end
+
+      factory :administrator do
+        administrator true
+      end
+    end
+  end
+end
--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -1,4 +1,173 @@
 require 'rails_helper'
 
 describe ArticlePolicy do
+  subject { ArticlePolicy.new(user, article) }
+
+  let(:resolved_scope) {
+    ArticlePolicy::Scope.new(user, Article.all).resolve
+  }
+
+  context "being a visitor" do
+    let(:user) { nil }
+    
+    context "accessing a published article" do
+      let(:article) {
+        FactoryGirl.create(:published_article_in_published_category) 
+      }
+
+      it "includes article in resolved scope" do
+        expect(resolved_scope).to include(article)
+      end
+
+      it { should permit_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+
+    context "accessing an unpublished article" do
+      let(:article) {
+        FactoryGirl.create(:unpublished_article_in_published_category) 
+      }
+
+      it "excludes article from resolved scope" do
+        expect(resolved_scope).not_to include(article)
+      end
+
+      it { should forbid_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+  end
+
+  context "being a registered user" do
+    let(:user) { FactoryGirl.create(:registered_user) }
+
+    context "accessing a published article" do
+      let(:article) {
+        FactoryGirl.create(:published_article_in_published_category) 
+      }
+
+      it "includes article in resolved scope" do
+        expect(resolved_scope).to include(article)
+      end
+
+      it { should permit_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+
+    context "accessing an unpublished article" do
+      let(:article) {
+        FactoryGirl.create(:unpublished_article_in_published_category) 
+      }
+
+      it "excludes article from resolved scope" do
+        expect(resolved_scope).not_to include(article)
+      end
+
+      it { should forbid_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+  end
+
+  context "being a contributor" do
+    let(:contributor_profile) {
+      FactoryGirl.create(:contributor_profile)
+    }
+    let(:user) {
+      FactoryGirl.create(
+        :contributor, 
+        contributor_profile: contributor_profile
+      )
+    }
+
+    context "accessing a published article" do
+      let(:article) {
+        FactoryGirl.create(:published_article_in_published_category) 
+      }
+
+      it "includes article in resolved scope" do
+        expect(resolved_scope).to include(article)
+      end
+
+      it { should permit_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+
+    context "accessing an unpublished article the user contributes to" do
+      let(:article) {
+        FactoryGirl.create(
+          :unpublished_article_in_published_category, 
+          contributions: [
+            Contribution.new(contributor_profile: contributor_profile)
+          ]
+        ) 
+      }
+
+      it "includes article in resolved scope" do
+        expect(resolved_scope).to include(article)
+      end
+
+      it { should permit_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+
+    context "accessing an unpublished article the user doesn't contribute to" do
+      let(:article) {
+        FactoryGirl.create(:unpublished_article_in_published_category) 
+      }
+
+      it "excludes article from resolved scope" do
+        expect(resolved_scope).not_to include(article)
+      end
+
+      it { should forbid_action(:show) }
+      it { should forbid_new_and_create }
+      it { should forbid_edit_and_update }
+      it { should forbid_action(:destroy) }
+    end
+  end
+
+  context "being an administrator" do
+    let(:user) { FactoryGirl.create(:administrator) }
+
+    context "accessing a published article" do
+      let(:article) {
+        FactoryGirl.create(:published_article_in_published_category) 
+      }
+
+      it "includes article in resolved scope" do
+        expect(resolved_scope).to include(article)
+      end
+
+      it { should permit_action(:show) }
+      it { should permit_new_and_create }
+      it { should permit_edit_and_update }
+      it { should permit_action(:destroy) }
+    end
+
+    context "accessing an unpublished article" do
+      let(:article) {
+        FactoryGirl.create(:unpublished_article_in_published_category) 
+      }
+
+      it "includes article in resolved scope" do
+        expect(resolved_scope).to include(article)
+      end
+
+      it { should permit_action(:show) }
+      it { should permit_new_and_create }
+      it { should permit_edit_and_update }
+      it { should permit_action(:destroy) }
+    end
+  end
 end
