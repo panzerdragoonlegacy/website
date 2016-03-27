25b577e9b1ac697cd561d02b773205d3b995e515
Chris Alley <chris@chrisalley.info>
2016-03-27 13:47:32 +1100

    Update action mailer settings to be agnostic about mail service

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,6 +1,6 @@
 Rails.application.config.action_mailer.smtp_settings = {
-  address:   "smtp.mandrillapp.com",
-  port:      587,
-  user_name: Rails.application.secrets.mandrill["username"],
-  password:  Rails.application.secrets.mandrill["api_key"]
+  address:   Rails.application.secrets.smtp_settings["address"],
+  port:      Rails.application.secrets.smtp_settings["port"],
+  user_name: Rails.application.secrets.smtp_settings["user_name"],
+  password:  Rails.application.secrets.smtp_settings["password"]
 }
