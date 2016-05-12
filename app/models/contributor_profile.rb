5c573b31ff19a67558d9a7896d297dd664080f38
Chris Alley <chris@chrisalley.info>
2016-05-13 00:29:32 +1000

    Resize embedded avatar and tidy up contributor profile presentation

--- app/models/contributor_profile.rb
+++ app/models/contributor_profile.rb
@@ -3,32 +3,33 @@ class ContributorProfile < ActiveRecord::Base
 
   has_one :user, dependent: :destroy
   has_many :news_entries, dependent: :destroy
-  
+
   has_many :contributions, dependent: :destroy
-  has_many :articles, through: :contributions, source: :contributable, 
+  has_many :articles, through: :contributions, source: :contributable,
     source_type: 'Article'
-  has_many :downloads, through: :contributions, source: :contributable, 
+  has_many :downloads, through: :contributions, source: :contributable,
     source_type: 'Download'
-  has_many :links, through: :contributions, source: :contributable, 
+  has_many :links, through: :contributions, source: :contributable,
     source_type: 'Link'
-  has_many :music_tracks, through: :contributions, source: :contributable, 
+  has_many :music_tracks, through: :contributions, source: :contributable,
     source_type: 'MusicTrack'
-  has_many :pictures, through: :contributions, source: :contributable, 
+  has_many :pictures, through: :contributions, source: :contributable,
     source_type: 'Picture'
-  has_many :poems, through: :contributions, source: :contributable, 
+  has_many :poems, through: :contributions, source: :contributable,
     source_type: 'Poem'
-  has_many :quizzes, through: :contributions, source: :contributable, 
+  has_many :quizzes, through: :contributions, source: :contributable,
     source_type: 'Quiz'
-  has_many :resources, through: :contributions, source: :contributable, 
+  has_many :resources, through: :contributions, source: :contributable,
     source_type: 'Resource'
-  has_many :stories, through: :contributions, source: :contributable, 
+  has_many :stories, through: :contributions, source: :contributable,
     source_type: 'Story'
-  has_many :videos, through: :contributions, source: :contributable, 
+  has_many :videos, through: :contributions, source: :contributable,
     source_type: 'Video'
 
   validates :name, presence: true, length: { in: 2..50 }, uniqueness: true
 
-  has_attached_file :avatar, styles: { thumbnail: "75x75#", embedded: "280x280>" },
+  has_attached_file :avatar,
+    styles: { thumbnail: "75x75#", embedded: "280x280#" },
     path: ":rails_root/public/system/:attachment/:id/:style/:avatar_filename",
     url: "/system/:attachment/:id/:style/:avatar_filename"
 
