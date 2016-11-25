3b6d9b17c457a77c052c009ab99b77b97e81e97f
Chris Alley <chris@chrisalley.info>
2016-11-26 00:06:25 +1100

    Add a confirmation dialog for when the user tries to publish a picture that is replacing another picture

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -7,4 +7,20 @@ $(document).on('ready page:load', function() {
   $('#picture_encyclopaedia_entry_ids').select2({
     tags: true
   });
+
+  $('.edit_picture').submit(function(event) {
+    if (
+      $('#picture_id_of_picture_to_replace').val() &&
+      $('#picture_publish').is(":checked")
+    ) {
+      if (!window.confirm(
+        "Are you sure you want to publish this picture, replacing " +
+        "'" + $('#picture_id_of_picture_to_replace option:selected').text() +
+        "' in the process? This destructive action cannot be undone. If " +
+        "you're sure, press the OK button for a new beginning..."
+      )) {
+        event.preventDefault();
+      };
+    }
+  });
 });
