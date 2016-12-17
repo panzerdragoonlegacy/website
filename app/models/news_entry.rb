e0584be747217aafe2378ef78050dbc3953bb845
Chris Alley <chris@chrisalley.info>
2016-12-17 13:06:31 +1100

    Refactor news entry publishing code into less lines/smaller methods

--- app/models/news_entry.rb
+++ app/models/news_entry.rb
@@ -12,35 +12,37 @@ class NewsEntry < ActiveRecord::Base
   def publish_news_entry
     # The first time the news entry is published:
     if self.published_at.blank? && self.publish
-      self.post_news_entry_to_twitter
+      self.publish_to_twitter
 
       # Set published_at field in the database to the current datetime:
       self.published_at = DateTime.now
     end
   end
 
-  def post_news_entry_to_twitter
+  def publish_to_twitter
     # Ensure that tweets aren't posted to Twitter when running RSpec:
     if Rails.env == 'production'
-      full_url = "http://www.panzerdragoonlegacy.com/news/" + self.url
-
-      # Get Twitter authentication details from secrets.yml
-      client = Twitter::REST::Client.new do |config|
-        config.consumer_key =
-          Rails.application.secrets.twitter["consumer_key"]
-        config.consumer_secret =
-          Rails.application.secrets.twitter["consumer_secret"]
-        config.access_token = Rails.application.secrets.twitter["oauth_token"]
-        config.access_token_secret =
-          Rails.application.secrets.twitter["oauth_token_secret"]
-      end
+      full_url = "http://www.panzerdragoonlegacy.com/news/#{self.url}"
 
       # Post the status update field + short URL to Twitter:
-      if self.name[-1] == "?" or self.name[-1] == "!"
-        client.update(self.name + " " + full_url)
+      if self.name[-1] == "?" || self.name[-1] == "!"
+        twitter_client.update(self.name + " " + full_url)
       else
-        client.update(self.name + ": " + full_url)
+        twitter_client.update(self.name + ": " + full_url)
       end
+    end
+  end
+
+  # Get Twitter authentication details from secrets.yml
+  def twitter_client
+    return Twitter::REST::Client.new do |config|
+      config.consumer_key =
+        Rails.application.secrets.twitter["consumer_key"]
+      config.consumer_secret =
+        Rails.application.secrets.twitter["consumer_secret"]
+      config.access_token = Rails.application.secrets.twitter["oauth_token"]
+      config.access_token_secret =
+        Rails.application.secrets.twitter["oauth_token_secret"]
     end
   end
 end
