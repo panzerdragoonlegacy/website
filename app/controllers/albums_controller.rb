61ee8eb6ea90baf4fd5af6a0ce7622ef035a9177
Chris Alley <chris@chrisalley.info>
2016-12-19 22:45:00 +1100

    Move loadable for album methods into a concern

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,6 @@
 class AlbumsController < ApplicationController
+  include LoadableForAlbum
+
   before_action :load_categories, except: [:index, :destroy]
   before_action :load_album, except: [:index, :new, :create]
 
@@ -56,36 +58,6 @@ class AlbumsController < ApplicationController
   def album_params
     params.require(:album).permit(
       policy(@album || :album).permitted_attributes
-    )
-  end
-
-  def load_categories
-    @categories = CategoryPolicy::Scope.new(
-      current_user,
-      Category.where(category_type: :picture).order(:name)
-    ).resolve
-  end
-
-  def load_album
-    @album = Album.find params[:id]
-    authorize @album
-  end
-
-  def load_contributors_albums
-    @contributor_profile = ContributorProfile.find_by(
-      url: params[:contributor_profile_id]
-    )
-    raise 'Contributor profile not found.' unless @contributor_profile
-    @albums = policy_scope(
-      Album.joins(:contributions).where(
-        contributions: { contributor_profile_id: @contributor_profile.id }
-      ).order(:name).page(params[:page])
-    )
-  end
-
-  def load_draft_albums
-    @albums = policy_scope(
-      Album.where(publish: false).order(:name).page(params[:page])
     )
   end
 
---
+++ app/controllers/concerns/loadable_for_album.rb
@@ -0,0 +1,35 @@
+module LoadableForAlbum
+  extend ActiveSupport::Concern
+
+  private
+
+  def load_categories
+    @categories = CategoryPolicy::Scope.new(
+      current_user,
+      Category.where(category_type: :picture).order(:name)
+    ).resolve
+  end
+
+  def load_album
+    @album = Album.find params[:id]
+    authorize @album
+  end
+
+  def load_contributors_albums
+    @contributor_profile = ContributorProfile.find_by(
+      url: params[:contributor_profile_id]
+    )
+    raise 'Contributor profile not found.' unless @contributor_profile
+    @albums = policy_scope(
+      Album.joins(:contributions).where(
+        contributions: { contributor_profile_id: @contributor_profile.id }
+      ).order(:name).page(params[:page])
+    )
+  end
+
+  def load_draft_albums
+    @albums = policy_scope(
+      Album.where(publish: false).order(:name).page(params[:page])
+    )
+  end
+end
