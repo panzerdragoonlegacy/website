498954e66c783814e11aed667876edb335d93ac3
Chris Alley <chris@chrisalley.info>
2017-03-13 15:05:17 +1100

    Permit information field when saving downloads

--- app/policies/download_policy.rb
+++ app/policies/download_policy.rb
@@ -65,6 +65,7 @@ class DownloadPolicy < ApplicationPolicy
       :category_id,
       :name,
       :description,
+      :information,
       :download,
       contributor_profile_ids: [],
       encyclopaedia_entry_ids: []
