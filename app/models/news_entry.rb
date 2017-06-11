9422a23b5dd09b4cd869ee724fe2745e532e977b
Chris Alley <chris@chrisalley.info>
2017-06-11 19:57:01 +1000

    Prevent horizontal scrollbar from appearing in news entries

--- app/models/news_entry.rb
+++ app/models/news_entry.rb
@@ -11,7 +11,7 @@ class NewsEntry < ActiveRecord::Base
   has_attached_file(
     :news_entry_picture,
     styles: {
-      embedded: '625x250#'
+      embedded: '622x250#'
     },
     path: ':rails_root/public/system/:attachment/:id/:style/:filename',
     url: '/system/:attachment/:id/:style/:filename'
