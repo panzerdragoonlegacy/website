43d4a96a9139fa9da9048533438b895673369c74
Chris Alley <chris@chrisalley.info>
2018-09-18 00:13:24 +1200

    Fix issues with album edit form

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,5 +1,5 @@
 $(document).on('ready page:load', function() {
   $('#album_category_id').select2();
   $('#album_contributor_profile_ids').select2();
-  $('#album_encyclopaedia_entry_ids').select2();
+  $('#album_tag_ids').select2();
 });
