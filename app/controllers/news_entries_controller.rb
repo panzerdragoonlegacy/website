c9a5d32960aae8a6d5f1d29dc6b6026bc89629a6
Chris Alley <chris@chrisalley.info>
2019-04-28 20:22:51 +1200

    Add news entry category selector to main page

--- app/controllers/news_entries_controller.rb
+++ app/controllers/news_entries_controller.rb
@@ -5,9 +5,18 @@ class NewsEntriesController < ApplicationController
     if params[:contributor_profile_id]
       load_contributors_news_entries
     else
-      @news_entries = policy_scope(
-        NewsEntry.order('published_at desc').page(params[:page])
-      )
+      if params[:category]
+        category = Category.find_by url: params[:category]
+        authorize category, :show?
+        @news_entries = policy_scope(
+          NewsEntry.where(category: category)
+            .order('published_at desc').page(params[:page])
+        )
+      else
+        @news_entries = policy_scope(
+          NewsEntry.order('published_at desc').page(params[:page])
+        )
+      end
     end
   end
 
