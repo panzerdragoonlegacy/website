3dec310178ee5093c6137359aba7e1c357be56aa
Chris Alley <chris@chrisalley.info>
2020-01-04 14:47:12 +1300

    Ensure that select2's work on page form

---
+++ app/assets/javascripts/page.js
@@ -0,0 +1,6 @@
+$(document).on('ready page:load', function() {
+  $('#page_category_id').select2();
+  $('#page_parent_page_id').select2();
+  $('#page_contributor_profile_ids').select2();
+  $('#page_tag_ids').select2();
+});
