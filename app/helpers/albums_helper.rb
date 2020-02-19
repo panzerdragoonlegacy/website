9536488dbaef5140fcc7b84e91e25fdb0a16e1e9
Chris Alley <chris@chrisalley.info>
2020-02-19 19:21:36 +1300

    Add previous/next in album links to pictures and videos

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,17 @@
 module AlbumsHelper
+  def previous_albumable(albumable)
+    albumables = ordered_albumables(albumable.album)
+    previous_index = albumables.index(albumable) - 1
+    albumables[previous_index] if previous_index >= 0
+  end
+
+  def next_albumable(albumable)
+    albumables = ordered_albumables(albumable.album)
+    next_index = albumables.index(albumable) + 1
+    albumables[next_index] if next_index < albumables.length
+  end
+
+  def ordered_albumables(album)
+    album.ordered_pictures + album.ordered_videos
+  end
 end
