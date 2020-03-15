eec2d6cda3dd7476c3ef38f891f77c378f067eb4
Chris Alley <chris@chrisalley.info>
2020-03-15 16:10:51 +1300

    Fix synchronisation of contributors/tags on album form

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -92,13 +92,13 @@ class Admin::AlbumsController < ApplicationController
         the_params[attributes_key][key]['information'] =
           the_params['information']
       end
-      if value['contributor_profile_ids'].blank?
+      if value['contributor_profile_ids'].reject { |cp| cp.blank? }.blank?
         the_params[attributes_key][key]['contributor_profile_ids'] =
-          the_params['contributor_profile_ids']
+          the_params['contributor_profile_ids'].reject { |cp| cp.blank? }
       end
-      if value['tag_ids'].blank?
-        the_params[attributes_key][key]['tag_ids'] =
-          the_params['tag_ids']
+      if value['tag_ids'].reject { |t| t.blank? }.blank?
+        the_params[attributes_key][key]['tag_ids'] <<
+          the_params['tag_ids'].reject { |t| t.blank? }
       end
     end
     the_params
