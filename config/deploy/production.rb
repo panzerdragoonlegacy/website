03b5c2968aad72df89bd51ac1440608894b8df71
Chris Alley <chris@chrisalley.info>
2020-06-01 14:05:52 +1200

    Update production server in deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '188.166.22.249', user: 'panzerdragoonlegacy', roles: %w{app web db}
+server '188.166.72.223', user: 'panzerdragoonlegacy', roles: %w{app web db}
 
 
 # role-based syntax
