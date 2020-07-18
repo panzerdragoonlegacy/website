6a9b470e7b69fe456932ab524f8b1d355a7d3116
Chris Alley <chris@chrisalley.info>
2020-07-18 22:34:54 +1200

    Add an SSL renewal script

---
+++ ssl_renew.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+DOCKER="/usr/bin/docker"
+
+cd /home/panzerdragoonlegacy/cms/
+$COMPOSE -f docker-compose.prod.yml run certbot renew
+$COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
