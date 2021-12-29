dd50210525d8d1c1db1d5ebf1438a8e7b3b1cedc
Chris Alley <chris@chrisalley.info>
2021-12-29 21:53:39 +1300

    Commit libv8 to lock file and precision to schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "url"
     t.string "description"
     t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.boolean "publish", default: false
     t.text "information"
-    t.datetime "published_at"
+    t.datetime "published_at", precision: 6
     t.string "source_url"
     t.string "instagram_post_id"
     t.index ["category_id"], name: "index_albums_on_category_id"
@@ -36,17 +36,17 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "description", limit: 255
     t.string "category_type", limit: 255
     t.boolean "publish", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.integer "category_group_id"
     t.integer "saga_id"
     t.string "short_name_for_saga"
     t.string "short_name_for_media_type"
-    t.datetime "published_at"
+    t.datetime "published_at", precision: 6
     t.string "category_picture_file_name"
     t.string "category_picture_content_type"
     t.integer "category_picture_file_size"
-    t.datetime "category_picture_updated_at"
+    t.datetime "category_picture_updated_at", precision: 6
     t.index ["saga_id"], name: "index_categories_on_saga_id"
   end
 
@@ -65,16 +65,16 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "name"
     t.string "url"
     t.string "category_group_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "contributions", id: :serial, force: :cascade do |t|
     t.integer "contributor_profile_id"
     t.integer "contributable_id"
     t.string "contributable_type", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "contributor_profiles", id: :serial, force: :cascade do |t|
@@ -84,17 +84,17 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "avatar_file_name", limit: 255
     t.string "avatar_content_type", limit: 255
     t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.datetime "avatar_updated_at", precision: 6
     t.string "website_url", limit: 255
     t.string "facebook_username", limit: 255
     t.string "twitter_username", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.string "discourse_username"
     t.boolean "publish", default: false
     t.string "description"
     t.string "website_name"
-    t.datetime "published_at"
+    t.datetime "published_at", precision: 6
     t.string "instagram_username"
     t.string "deviantart_username"
     t.string "discord_user_id"
@@ -109,16 +109,16 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "download_file_name", limit: 255
     t.string "download_content_type", limit: 255
     t.integer "download_file_size"
-    t.datetime "download_updated_at"
+    t.datetime "download_updated_at", precision: 6
     t.boolean "publish", default: false
     t.integer "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.string "download_picture_file_name"
     t.string "download_picture_content_type"
     t.integer "download_picture_file_size"
-    t.datetime "download_picture_updated_at"
-    t.datetime "published_at"
+    t.datetime "download_picture_updated_at", precision: 6
+    t.datetime "published_at", precision: 6
   end
 
   create_table "illustrations", id: :serial, force: :cascade do |t|
@@ -126,9 +126,9 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "illustration_file_name", limit: 255
     t.string "illustration_content_type", limit: 255
     t.integer "illustration_file_size"
-    t.datetime "illustration_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "illustration_updated_at", precision: 6
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "music_tracks", id: :serial, force: :cascade do |t|
@@ -139,21 +139,21 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "mp3_music_track_file_name", limit: 255
     t.string "mp3_music_track_content_type", limit: 255
     t.integer "mp3_music_track_file_size"
-    t.datetime "mp3_music_track_updated_at"
+    t.datetime "mp3_music_track_updated_at", precision: 6
     t.boolean "publish", default: false
     t.integer "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.integer "track_number", default: 0
     t.string "flac_music_track_file_name", limit: 255
     t.string "flac_music_track_content_type", limit: 255
     t.integer "flac_music_track_file_size"
-    t.datetime "flac_music_track_updated_at"
+    t.datetime "flac_music_track_updated_at", precision: 6
     t.string "music_track_picture_file_name"
     t.string "music_track_picture_content_type"
     t.integer "music_track_picture_file_size"
-    t.datetime "music_track_picture_updated_at"
-    t.datetime "published_at"
+    t.datetime "music_track_picture_updated_at", precision: 6
+    t.datetime "published_at", precision: 6
   end
 
   create_table "news_entries", id: :serial, force: :cascade do |t|
@@ -161,14 +161,14 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "url", limit: 255
     t.text "content"
     t.integer "contributor_profile_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "published_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.datetime "published_at", precision: 6
     t.boolean "publish", default: false
     t.string "news_entry_picture_file_name"
     t.string "news_entry_picture_content_type"
     t.integer "news_entry_picture_file_size"
-    t.datetime "news_entry_picture_updated_at"
+    t.datetime "news_entry_picture_updated_at", precision: 6
     t.string "summary"
   end
 
@@ -186,10 +186,10 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "page_picture_file_name"
     t.string "page_picture_content_type"
     t.integer "page_picture_file_size"
-    t.datetime "page_picture_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "published_at"
+    t.datetime "page_picture_updated_at", precision: 6
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.datetime "published_at", precision: 6
     t.string "old_model_type"
     t.integer "old_model_id"
   end
@@ -202,15 +202,15 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "picture_file_name", limit: 255
     t.string "picture_content_type", limit: 255
     t.integer "picture_file_size"
-    t.datetime "picture_updated_at"
+    t.datetime "picture_updated_at", precision: 6
     t.boolean "publish", default: false
     t.integer "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.integer "id_of_picture_to_replace"
     t.integer "album_id"
     t.string "source_url"
-    t.datetime "published_at"
+    t.datetime "published_at", precision: 6
     t.boolean "full_size_link", default: true
     t.integer "sequence_number", default: 0
     t.boolean "controversial_content", default: false
@@ -221,15 +221,15 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.integer "quiz_question_id"
     t.text "content"
     t.boolean "correct_answer", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "quiz_questions", id: :serial, force: :cascade do |t|
     t.integer "quiz_id"
     t.text "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "quizzes", id: :serial, force: :cascade do |t|
@@ -237,9 +237,9 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "url", limit: 255
     t.string "description", limit: 255
     t.boolean "publish", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "published_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.datetime "published_at", precision: 6
     t.integer "category_id"
   end
 
@@ -247,8 +247,8 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "old_url"
     t.string "new_url"
     t.text "comment"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   create_table "resources", id: :serial, force: :cascade do |t|
@@ -257,25 +257,25 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.text "content"
     t.boolean "publish", default: false
     t.integer "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "published_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.datetime "published_at", precision: 6
   end
 
   create_table "sagas", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.integer "sequence_number", default: 1
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "tag_id"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "taggable_id"
     t.string "taggable_type", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "tag_id"
   end
 
@@ -283,12 +283,12 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "name"
     t.string "url"
     t.string "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.string "tag_picture_file_name"
     t.string "tag_picture_content_type"
     t.integer "tag_picture_file_size"
-    t.datetime "tag_picture_updated_at"
+    t.datetime "tag_picture_updated_at", precision: 6
     t.text "information"
     t.string "wiki_slug"
   end
@@ -297,22 +297,22 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: 6
+    t.datetime "last_sign_in_at", precision: 6
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: 6
+    t.datetime "confirmation_sent_at", precision: 6
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "locked_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.boolean "administrator", default: false
     t.integer "contributor_profile_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
@@ -327,7 +327,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "event", null: false
     t.string "whodunnit"
     t.text "object"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: 6
     t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
@@ -340,17 +340,17 @@ ActiveRecord::Schema.define(version: 2021_12_01_070122) do
     t.string "mp4_video_file_name", limit: 255
     t.string "mp4_video_content_type", limit: 255
     t.integer "mp4_video_file_size"
-    t.datetime "mp4_video_updated_at"
+    t.datetime "mp4_video_updated_at", precision: 6
     t.boolean "publish", default: false
     t.integer "category_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
     t.string "youtube_video_id", limit: 255
     t.string "video_picture_file_name"
     t.string "video_picture_content_type"
     t.integer "video_picture_file_size"
-    t.datetime "video_picture_updated_at"
-    t.datetime "published_at"
+    t.datetime "video_picture_updated_at", precision: 6
+    t.datetime "published_at", precision: 6
     t.string "source_url"
     t.integer "album_id"
     t.integer "sequence_number", default: 0
