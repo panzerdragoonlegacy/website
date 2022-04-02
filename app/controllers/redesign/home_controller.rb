52ae6abb6076575de173d485ac6af0332bf4574f
Chris Alley <chris@chrisalley.info>
2022-04-02 14:40:53 +1300

    Display home page even if the user is not authorised

--- app/controllers/redesign/home_controller.rb
+++ app/controllers/redesign/home_controller.rb
@@ -2,23 +2,23 @@ class Redesign::HomeController < ApplicationController
   layout 'redesign'
 
   def show
-    @featured_news_entry = NewsEntry.order('published_at desc').first
-    authorize @featured_news_entry
+    @featured_news_entry =
+      policy_scope(NewsEntry.order('published_at desc')).first
     @pictures = policy_scope(Picture.order('published_at desc').limit(4))
-    @page = Page.where(page_type: :literature).order('published_at desc').first
-    authorize @page
-    @music_track = MusicTrack.order('published_at desc').first
-    authorize @music_track
-    @video = Video.order('published_at desc').first
-    authorize @video
-    @download = Download.order('published_at desc').first
-    authorize @download
-    @news_entries =
-      policy_scope(
-        NewsEntry
-          .where.not(id: @featured_news_entry.id)
-          .order(published_at: :desc)
-          .page(params[:page])
-      )
+    @page = policy_scope(
+      Page.where(page_type: :literature).order('published_at desc')
+    ).first
+    @music_track = policy_scope(MusicTrack.order('published_at desc')).first
+    @video = policy_scope(Video.order('published_at desc')).first
+    @download = policy_scope(Download.order('published_at desc')).first
+    if @featured_news_entry.present?
+      @news_entries =
+        policy_scope(
+          NewsEntry
+            .where.not(id: @featured_news_entry.id)
+            .order(published_at: :desc)
+            .page(params[:page])
+        )
+    end
   end
 end
