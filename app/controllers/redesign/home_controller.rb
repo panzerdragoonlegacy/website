c2d3a2757e3b099201791e86864edea9e85b3cd3
Chris Alley <chris@chrisalley.info>
2022-04-12 21:18:05 +1200

    Only show published content on home page for consistency with contributions feed

--- app/controllers/redesign/home_controller.rb
+++ app/controllers/redesign/home_controller.rb
@@ -3,18 +3,47 @@ class Redesign::HomeController < ApplicationController
 
   def show
     @featured_news_entry =
-      policy_scope(NewsEntry.order('published_at desc')).first
-    @pictures = policy_scope(Picture.order('published_at desc').limit(4))
-    @page = policy_scope(
-      Page.where(page_type: :literature).order('published_at desc')
-    ).first
-    @music_track = policy_scope(MusicTrack.order('published_at desc')).first
-    @video = policy_scope(Video.order('published_at desc')).first
-    @download = policy_scope(Download.order('published_at desc')).first
+      policy_scope(
+        NewsEntry
+          .where(publish: true)
+          .order('published_at desc')
+      ).first
+    @pictures =
+      policy_scope(
+        Picture
+          .where(publish: true)
+          .order('published_at desc')
+          .limit(4)
+      )
+    @page =
+      policy_scope(
+        Page
+          .where(page_type: :literature, publish: true)
+          .order('published_at desc')
+      ).first
+    @music_track =
+      policy_scope(
+        MusicTrack
+          .where(publish: true)
+          .order('published_at desc')
+      ).first
+    @video =
+      policy_scope(
+        Video
+          .where(publish: true)
+          .order('published_at desc')
+      ).first
+    @download =
+      policy_scope(
+        Download
+          .where(publish: true)
+          .order('published_at desc')
+      ).first
     if @featured_news_entry.present?
       @news_entries =
         policy_scope(
           NewsEntry
+            .where(publish: true)
             .where.not(id: @featured_news_entry.id)
             .order(published_at: :desc)
             .page(params[:page])
