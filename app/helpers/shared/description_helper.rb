027595186dfc9e9becf8da1b9a85b775be791546
Chris Alley <chris@chrisalley.info>
2022-06-08 21:08:19 +1200

    Ensure that descriptions still show up if there is only one description

--- app/helpers/shared/description_helper.rb
+++ app/helpers/shared/description_helper.rb
@@ -3,6 +3,6 @@ module Shared::DescriptionHelper
     identical_describables = describables.select do |describable|
       describable.description == describables[0].description
     end
-    describables.count == identical_describables.count
+    describables.count == identical_describables.count && describables.count > 1
   end
 end
