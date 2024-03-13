8c5582cadec53065e9cf918f5f74f23897bdc0ea
Chris Alley <chris@chrisalley.info>
2024-03-14 05:07:39 +1300

    Update video source URL logic to handle links to X

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,6 +1,8 @@
 module VideosHelper
   def video_source_link_text(source_url)
-    return 'Twitter' if source_url.include? 'twitter.com'
+    if source_url.include?('twitter.com') || source_url.include?('x.com')
+      return 'X (Twitter)'
+    end
     return 'YouTube' if source_url.include? 'youtube.com'
     'External Webpage'
   end
